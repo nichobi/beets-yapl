d1179f9fc86a658bbc1eff72fa45850d4354a0ba
Nicholas Boyd Isacsson <nicholas@isacsson.se>
2022-11-27 18:15:58 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+setup(
+    name='beets-yapl',
+    version='0.1.0',
+    description='beets plugin to handle yaml playlists',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Nicholas Boyd Isacsson',
+    author_email='nicholas@isacsson.se',
+    url='http://www.github.com/nichobi/beets-yapl',
+    license='GPL3',
+    platforms='ALL',
+
+    test_suite='test',
+
+    packages=['beetsplug'],
+
+    install_requires=[
+        'beets>=1.4.7',
+    ],
+
+    classifiers=[
+        'Topic :: Multimedia :: Sound/Audio',
+        'Topic :: Multimedia :: Sound/Audio :: Players :: MP3',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Environment :: Console',
+        'Programming Language :: Python :: 3',
+    ],
+    )
+
